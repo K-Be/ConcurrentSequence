79fbc7cf5cce9049bbdacdd926ba3871e2cee9ac
Andrew Romanov <scalli-k-be@ya.ru>
2019-10-12 13:44:41 +0700

    initial commit

---
+++ RAVConcurrentContainters/RAVConcurrentContainters.h
@@ -0,0 +1,19 @@
+//
+//  RAVConcurrentContainters.h
+//  RAVConcurrentContainters
+//
+//  Created by Andrew Romanov on 12.10.2019.
+//  Copyright © 2019 Andrew Romanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RAVConcurrentContainters.
+FOUNDATION_EXPORT double RAVConcurrentContaintersVersionNumber;
+
+//! Project version string for RAVConcurrentContainters.
+FOUNDATION_EXPORT const unsigned char RAVConcurrentContaintersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RAVConcurrentContainters/PublicHeader.h>
+
+
