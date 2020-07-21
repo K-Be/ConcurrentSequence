77e7b9954afaed8773e5cccb9c0ca78505783053
Andrew Romanov <79236447723@ya.ru>
2020-07-21 15:54:58 +0700

    added test for Lock

---
+++ RAVConcurrentContainersTests/LockTest.swift
@@ -0,0 +1,30 @@
+//
+//  LockTest.swift
+//  RAVConcurrentContainersTests
+//
+//  Created by Andrew Romanov on 21.07.2020.
+//  Copyright © 2020 Andrew Romanov. All rights reserved.
+//
+
+import XCTest
+@testable import RAVConcurrentContainers
+
+class LockTest: XCTestCase {
+
+    func testLock() throws {
+        let lock = Lock()
+        let operationQueue = OperationQueue()
+        var val = 0
+        let numberOfOperations = 1000
+        
+        for _ in 1...numberOfOperations {
+            operationQueue.addOperation {
+                lock.sync {
+                    val += 1
+                }
+            }
+        }
+        
+        XCTAssertEqual(val, numberOfOperations)
+    }
+}
